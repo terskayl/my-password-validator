dce84f0caa5e446ac73a4fc47ccef63a7a05f2e9
Aaron Jiang <97208428+terskayl@users.noreply.github.com>
2025-03-25 12:51:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flask
 
 
 # TODO: change this to your academic email
-AUTHOR = "lumbroso@seas.upenn.edu"
+AUTHOR = "aajiang@seas.upenn.edu"
 
 
 app = flask.Flask(__name__)
@@ -25,5 +25,16 @@ def check_password():
     data = flask.request.get_json() or {}
     pw = data.get("password", "")
 
-    # FIXME: to be implemented
-    return flask.jsonify({"valid": False, "reason": "Not implemented"}), 501
+
+    # Length >= 8, 1 upper case, 1 digit, 1 special character
+    if len(pw) < 8:
+        return flask.jsonify({"valid": False, "reason": "too short"}), 501 
+    if len([c for c in list(pw) if c in "!@#$%^&*"]) < 1:
+        return flask.jsonify({"valid": False, "reason": "no special characters"}), 501 
+    if len([c for c in list(pw) if c.isupper()]) < 1:
+        return flask.jsonify({"valid": False, "reason": "no uppercase letters"}), 501 
+    if len([c for c in list(pw) if c.isdigit()]) < 1:
+        return flask.jsonify({"valid": False, "reason": "no digits"}), 501 
+
+
+    return flask.jsonify({"valid": True, "reason": "Valid Password"}), 501
