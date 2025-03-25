bc9c9c4e5dd7060f2da7deced5f87ed4c24d2ef3
Aaron Jiang <97208428+terskayl@users.noreply.github.com>
2025-03-25 13:00:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ def check_password():
 
     # Length >= 8, 1 upper case, 1 digit, 1 special character
     if len(pw) < 8:
-        return flask.jsonify({"valid": False, "reason": "too short"}), 501 
+        return flask.jsonify({"valid": False, "reason": "too short"}), 200 
     if len([c for c in list(pw) if c in "!@#$%^&*"]) < 1:
-        return flask.jsonify({"valid": False, "reason": "no special characters"}), 501 
+        return flask.jsonify({"valid": False, "reason": "no special characters"}), 200 
     if len([c for c in list(pw) if c.isupper()]) < 1:
-        return flask.jsonify({"valid": False, "reason": "no uppercase letters"}), 501 
+        return flask.jsonify({"valid": False, "reason": "no uppercase letters"}), 200 
     if len([c for c in list(pw) if c.isdigit()]) < 1:
-        return flask.jsonify({"valid": False, "reason": "no digits"}), 501 
+        return flask.jsonify({"valid": False, "reason": "no digits"}), 200 
 
 
-    return flask.jsonify({"valid": True, "reason": "Valid Password"}), 501
+    return flask.jsonify({"valid": True, "reason": "Valid Password"}), 200
